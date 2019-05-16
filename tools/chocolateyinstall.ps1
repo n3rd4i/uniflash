80fc1f1f744e64f0079c9f46735d6552fcf3bade
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-05-16 11:36:59 +0200

    updated spec to contain source

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://software-dl.ti.com/ccs/esd/uniflash/uniflash_sl.5.0.0.2289.exe' # download url, HTTPS preferred
+$url        = 'http://software-dl.ti.com/ccs/esd/uniflash/uniflash_sl.5.0.0.2289.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe'
   url           = $url
-  softwareName  = 'uniflash*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  # e.g. checksum -t sha256 -f path\to\file
+  softwareName  = 'uniflash*'
   checksum      = '7FCE502D807C9859E64A715E29648E97'
-  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
-  # silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  silentArgs    = "--unattendedmodeui minimal --mode unattended" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  checksumType  = 'md5'
+  silentArgs    = "--unattendedmodeui none --mode unattended"
   validExitCodes= @(0, 3010, 1641)
 }
 

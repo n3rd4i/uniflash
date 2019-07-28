3622478677088b0914f12c6bd380ab77e2ffb6c2
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-07-28 20:38:43 +0200

    always try to accept certificates regardless of how many

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-. "$toolsDir\helper.ps1"
+. "$(Join-Path $toolsDir helper.ps1)"
 
 $url        = 'http://software-dl.ti.com/ccs/esd/uniflash/uniflash_sl.5.0.0.2289.exe'
 $packageArgs = @{
@@ -14,19 +14,7 @@ $packageArgs = @{
   silentArgs    = "--unattendedmodeui minimal --mode unattended"
   validExitCodes= @(0, 3010, 1641)
 }
-
-$certificates = @{
-  C76E488AFC115AD09DA669CBE413316FE56E9438 = 'CN=Texas Instruments, OU=WCS, O=Texas Instruments, L=Dallas, S=Texas, C=US';
-  BCCCF26267CEE6B58B3818782D26F4307BCB6A4F = 'CN=Texas Instruments Inc, OU=Stellaris MCU, OU=Digital ID Class 3 - Microsoft Software Validation v2, O=Texas Instruments Inc, L=Austin, S=Texas, C=US';
-  EE0C8BA018CE926432C3CC23523A34073D01954A = 'CN=Texas Instruments Incorporated, OU=Digital ID Class 3 - Microsoft Software Validation v2, O=Texas Instruments Incorporated, L=Dallas, S=Texas, C=US';
-  FEFF132CF126562676138326AD817697BC5F81D6 = 'CN=Texas Instruments Incorporated, O=Texas Instruments Incorporated, L=Sugar Land, S=Texas, C=US';
-}
-# foreach($key in $certificates.Keys){ dir Cert:\CurrentUser\TrustedPublisher\ -Recurse | ? thumbprint -match $key | Remove-Item -WhatIf }
-$installedCertificates = foreach($key in $certificates.Keys){ Get-ChildItem Cert:\CurrentUser\TrustedPublisher\|Where thumbprint -eq $key }
-$nbrNeededCertificates = $certificates.count - $installedCertificates.count
-if ($nbrNeededCertificates) {
-  $ahkProc = Start-Ahk('chocolateyInstallCertificate.ahk')
-}
+$ahkProc = Start-Ahk('cert_installer.ahk')
 
 Install-ChocolateyPackage @packageArgs
 

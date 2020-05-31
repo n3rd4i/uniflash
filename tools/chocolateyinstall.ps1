f835bb5384feef5b3d4f71c16f8c8096292b491b
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-31 15:33:38 +0200

    update to version 6.0.0.2710

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir helper.ps1)"
 
-$url        = 'http://software-dl.ti.com/ccs/esd/uniflash/uniflash_sl.5.1.0.2397.exe'
+$url        = 'http://software-dl.ti.com/ccs/esd/uniflash/uniflash_sl.6.0.0.2710.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
   softwareName  = 'uniflash*'
-  checksum      = '4EDD650A39F1227F4B2EACA6840A0487FDAFE8AC70917C3EEA686173D5118054'
+  checksum      = '4081E62EED5FAB178FE7B7631EB90853F6F466F37F447050D765A8C91ACE073F'
   checksumType  = 'sha256'
   silentArgs    = "--unattendedmodeui minimal --mode unattended"
   validExitCodes= @(0, 3010, 1641)
